cfc42c902b74937c5baa056b66a32de459353c80
Naveen.S <michealtina919>
2018-11-21 01:34:31 +0530

    Nunit

---
+++ NUnit_Selenium/FeedbackFeature.cs
@@ -0,0 +1,57 @@
+﻿using NUnit.Framework;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using System;
+namespace NUnit_Selenium
+{
+    [TestFixture]
+    public class FeedbackFeature
+    {
+        IWebDriver driver;
+        [OneTimeSetUp]
+        public void onetimesetup()
+        {
+            driver = new ChromeDriver();
+            driver.Manage().Window.Maximize();
+            driver.Url = "http://zero.webappsecurity.com/index.html";
+        }
+ 
+        [SetUp]
+        public void setup()
+        {
+            driver.FindElement(By.XPath("//li[@id='feedback']")).Click();
+            Assert.AreEqual("Zero - Contact Us", driver.Title);
+        }
+
+        [Test]
+        public void SubmitFeedback()
+        {
+            driver.FindElement(By.XPath("//*[@id='name']")).SendKeys("Sankar");
+            driver.FindElement(By.XPath("//*[@id='email']")).SendKeys("sankar2.0@gmail.com");
+            driver.FindElement(By.XPath("//*[@id='subject']")).SendKeys("LoginIssue");
+            driver.FindElement(By.XPath("//*[@id='comment']")).SendKeys("Not able to login");
+            driver.FindElement(By.Name("submit")).Click();
+            Assert.AreEqual(true, driver.FindElement(By.XPath("//div[@class='offset3 span6']")).Text.Contains("Sankar"));
+        }
+
+        [Test]
+        public void FAQ()
+        {
+            driver.FindElement(By.XPath("//*[@id='faq-link']")).Click();
+            Assert.AreEqual("Zero - FAQ - Frequently Asked Questions", driver.Title);
+
+        }
+
+        [TearDown]
+        public void teardown()
+        {
+            driver.FindElement(By.XPath("//a[contains(text(),'Zero Bank')]")).Click();
+        }
+
+        [OneTimeTearDown]
+        public void onetimeteardown()
+        {
+            //driver.Close();
+        }
+    }
+}
---
+++ NUnit_Selenium/NUnitSnippet.cs
@@ -0,0 +1,100 @@
+﻿using NUnit.Framework;
+using System;
+using System.Threading;
+
+namespace NUnit_Selenium
+{
+    [TestFixture]
+    [Author("Naveen.S", "naveens@deltaww.com")]//Multiple Author details also allowed
+    [Description("Nunit Attribute Snippet explaination")]
+    public class NUnitSnippet
+    {
+        [OneTimeSetUp]
+        public void onetimesetup()
+        {
+            Console.WriteLine("OneTimeSetUp");
+        }
+ 
+        [SetUp]
+        public void setup()
+        {
+            Console.WriteLine("Setup");
+        }
+
+        [Test]
+        public void TestCase1()
+        {
+            Console.WriteLine("Test Case 1");
+        }
+
+        [Test, Explicit]
+        public void TestCase2()
+        {
+            Console.WriteLine("Test Case 2");
+        }
+
+        [Test, Combinatorial]
+        public void TestCase3(
+        [Values(1, 2, 3)] int x,
+        [Values("A", "B")] string s)
+        {
+            Console.WriteLine("Test Case 3 -- Value: " + x + s);
+        }
+
+        [Test]
+        [Ignore("This testcase is Ignored")]
+        public void TestCase4()
+        {
+            Console.WriteLine("Test Case 4");
+        }
+
+        [Test]
+        [Ignore("Waiting for Developer to fix the bugs", Until = "2018-11-21 01:22:00Z")]
+        public void TestCase5()
+        {
+            Console.WriteLine("Test Case 5");
+        }
+
+        [Test, MaxTime(20)] //milliseconds 
+        public void TestCase6()
+        {
+            Thread.Sleep(25);
+            Console.WriteLine("Test Case 6");
+        }
+
+        [Test, Order(3)]
+        public void TestCase7()
+        {
+            Thread.Sleep(25);
+            Console.WriteLine("Test Case 6");
+        }
+
+        [Test, Order(1)]
+        public void TestCase8()
+        {
+            Thread.Sleep(25);
+            Console.WriteLine("Test Case 6");
+        }
+
+        [Test, Order(2)]
+        public void TestCase9()
+        {
+            Thread.Sleep(25);
+            Console.WriteLine("Test Case 6");
+        }
+
+
+
+        [TearDown]
+        public void teardown()
+        {
+            Console.WriteLine("TearDown");
+        }
+
+        [OneTimeTearDown]
+        public void onetimeteardown()
+        {
+            Console.WriteLine("OneTimeTearDown");
+        }
+    }
+}
